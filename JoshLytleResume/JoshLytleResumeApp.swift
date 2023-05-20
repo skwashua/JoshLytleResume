6ca5b56fcd596962a3c070296b0201207a0b7d56
skwashua <josh@whattheblue.com>
2023-05-20 08:18:33 -0600

    Initial Commit

---
+++ JoshLytleResume/JoshLytleResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshLytleResumeApp.swift
+//  JoshLytleResume
+//
+//  Created by Joshua Lytle on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct JoshLytleResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
